a2a1317cc6ab543ca3f31b3893fadbe5d2b5a9cb
Chrono <aioncalyx@gmail.com>
2022-07-20 18:59:51 -0300

    estilo tabela rota

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -9,9 +9,38 @@ for (let i = 0; i < tabs.length; i++) {
             for (let j = 0; j < contents.length; j++) {
                 if (!contents[j].classList.contains('d-none')) {
                     contents[j].classList.add('d-none')
-                    console.log('here')
                 }
             }
+
+            for (let k = 0; k < tabs.length; k++) {
+                tabs[k].classList.remove('bg-orange')
+            }
+
+            tabs[i].classList.add('bg-orange')
+
+            content.classList.toggle('d-none')
+    })
+}
+
+let tabs2 = document.querySelectorAll('.toggle-tab')
+
+for (let i = 0; i < tabs2.length; i++) {
+    tabs2[i].addEventListener('click', () => {
+        let tabID = tabs2[i].getAttribute('href'),
+            contents = document.querySelectorAll('.tab-pane2'),
+            content = document.querySelector(tabID)
+
+            for (let j = 0; j < contents.length; j++) {
+                if (!contents[j].classList.contains('d-none')) {
+                    contents[j].classList.add('d-none')
+                }
+            }
+
+            for (let k = 0; k < tabs2.length; k++) {
+                tabs2[k].classList.remove('bg-orange')
+            }
+
+            tabs2[i].classList.add('bg-orange')
 
             content.classList.toggle('d-none')
     })
